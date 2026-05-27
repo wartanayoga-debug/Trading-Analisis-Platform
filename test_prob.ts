550c70a0b818b5e498875a15dd2a7c3ca9f5b5f6
wartanayoga-debug <wartanayoga@gmail.com>
2026-05-28 00:28:20 +0900

    chore: remove obsolete test and experiment files

--- test_prob.ts
+++
@@ -1,7 +0,0 @@
-import { RandomForestClassifier } from "ml-random-forest";
-const rf = new RandomForestClassifier({ seed: 42, maxFeatures: 1.0, replacement: true, nEstimators: 10 });
-const dummyX = [[30, 0, -1, 0.2], [70, 1, 1, 0.8], [50, 0, 0, 0.5], [60, 1, 0.5, 0.7], [40, 0, -0.5, 0.3], [80, 1, 1.2, 0.9]];
-const dummyY = [0, 1, 0, 1, 0, 1];
-rf.train(dummyX, dummyY);
-console.log("Prediction probs:", rf.predictProbability([[55, 0.5, 0.2, 0.6]]));
-console.log("Prediction probs2:", rf.predictProbability([[80, 1, 1.2, 0.9]]));
--- test_rf4.ts
+++
@@ -1,7 +0,0 @@
-import { RandomForestClassifier } from 'ml-random-forest';
-const rf = new RandomForestClassifier({ seed: 42 });
-const dummyX = [[30, 0, -1, 0.2], [70, 1, 1, 0.8], [50, 0, 0, 0.5], [60, 1, 0.5, 0.7]];
-const dummyY = [0, 1, 0, 1];
-rf.train(dummyX, dummyY);
-console.log("Prediction 1:", rf.predict([[30, 0, -1, 0.2]]));
-console.log("Prediction 2:", rf.predict([[70, 1, 1, 0.8]]));
